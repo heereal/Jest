8920745166f7b7065568071f9318bd84de5a1e79
heeryoung <winterbooks@naver.com>
2023-11-22 11:52:21 +0900

    add: jest test code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Jest
-Jest를 공부하기 위해 만든 레파지토리입니다.
+
+[Jest 공식문서](https://jestjs.io/docs/getting-started)를 통해 공부한 내용을 기록한 레파지토리입니다.
---
+++ tests/common.test.js
@@ -0,0 +1,15 @@
+// toBe uses Object.is to test exact equality.
+// If you want to check the value of an object, use toEqual.
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
