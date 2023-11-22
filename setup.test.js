b14d4475e9182f309b6f6181782370fa9d5ab179
heeryoung <winterbooks@naver.com>
2023-11-22 14:06:30 +0900

    add: setup and teardown

---
+++ setup.test.js
@@ -0,0 +1,82 @@
+// Repeating Setup
+// The top level before* and after* hooks apply to every test in a file.
+beforeEach(() => {
+  initializeCityDatabase();
+});
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+// One-Time Setup
+beforeAll(() => {
+  return initializeCityDatabase();
+});
+
+afterAll(() => {
+  return clearCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+// Scoping
+// The hooks declared inside a describe block apply only to the tests within that describe block.
+describe("matching cities to foods", () => {
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test("Vienna <3 veal", () => {
+    expect(isValidCityFoodPair("Vienna", "Wiener Schnitzel")).toBe(true);
+  });
+
+  test("San Juan <3 plantains", () => {
+    expect(isValidCityFoodPair("San Juan", "Mofongo")).toBe(true);
+  });
+});
+
+// Order of Execution
+describe("describe outer", () => {
+  console.log("describe outer-a");
+
+  describe("describe inner 1", () => {
+    console.log("describe inner 1");
+
+    test("test 1", () => console.log("test 1"));
+  });
+
+  console.log("describe outer-b");
+
+  test("test 2", () => console.log("test 2"));
+
+  describe("describe inner 2", () => {
+    console.log("describe inner 2");
+
+    test("test 3", () => console.log("test 3"));
+  });
+
+  console.log("describe outer-c");
+});
+
+// describe outer-a
+// describe inner 1
+// describe outer-b
+// describe inner 2
+// describe outer-c
+// test 1
+// test 2
+// test 3
